57038bebba1b59bf3d2418abaf90838c0f6c059f
Andy Moran <49261838+carlit-os@users.noreply.github.com>
2021-05-30 22:43:07 -0500

    convert for each to promise breadcrumb

--- article_parse.js
+++ article_parse.js
@@ -3,32 +3,42 @@ const Article = require('newspaperjs').Article
 const zips= require("./public/data/helpers")
 const zips_data = zips.elpaso_towns_zips
 
+// const myAsyncLoopFunction = async (array, ) {
+//     const allAsyncResults = []
+  
+//     for (const item of array) {
+//       const asnycResult = await asyncFunction(item)
+//       allAsyncResults.push(asyncResult)
+//     }
+  
+//     return allAsyncResults
+// }
+
 
 const test = () =>{
-    // Build.getCategoriesUrl('https://www.nytimes.com', ['politics', 'sports', 'technology']).then(categories=>{
-    //     //console.log(categories); 
-    // }).catch(reason=>{
-    //     //console.log(reason);
-    // })
     return Build.getArticlesUrl('https://www.ktsm.com').then(result=>{
         const zips_set = []
         
         const town_names = Object.keys(zips_data)
 
-        result.forEach(link => { //parse article
+        for await (const link of result){ //parse article
             Article(link)
             .then(nlp=>{
-                Object.keys(zips_data).forEach(name => {
-                    const search = nlp.text.search(RegExp(`${name}`,"i"))
-
-                    console.log(search)
+                town_names.forEach(name => {
+                    const town_idx = nlp.text.search(RegExp(`${name}`,"i"))
+                    if(town_idx != -1){
+                    zips_set.push(town_idx)
+                    console.log("added")
+                }
                 })
-                //console.log(result.text);
-            }).catch(reason=>{
+            }) 
+            .catch(reason=>{
                 console.log(reason);
-            })
-        });
+            })  
+        }
+        console.log(zips_set)
 
+        
         //return articls
     }).catch(reason=>{
         console.log(reason)
