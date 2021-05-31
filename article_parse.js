f00f9818c44a4e2cd8e242c33e296d1c85e7c862
Andy Moran <49261838+carlit-os@users.noreply.github.com>
2021-05-30 23:41:20 -0500

    includes searching

--- article_parse.js
+++ article_parse.js
@@ -3,19 +3,17 @@ const Article = require('newspaperjs').Article
 const zips= require("./public/data/helpers")
 const zips_data = zips.elpaso_towns_zips
 
-// const myAsyncLoopFunction = async (array, ) {
-//     const allAsyncResults = []
-  
-//     for (const item of array) {
-//       const asnycResult = await asyncFunction(item)
-//       allAsyncResults.push(asyncResult)
+// const article_search = async (nlp, town_names) => {
+//     for(const name of town_names){
+//         const town_idx = nlp.text.search(RegExp(`${name}`,"i"))
+//         if(town_idx != -1){
+//         zips_set.push(town_idx)
+//         console.log("added")
 //     }
-  
-//     return allAsyncResults
-// }
+// }}
 
 
-const test = () =>{
+const test = async () =>{
     return Build.getArticlesUrl('https://www.ktsm.com').then(result=>{
         const zips_set = []
         
@@ -24,18 +22,15 @@ const test = () =>{
         result.forEach(link => { //parse article
             Article(link)
             .then(nlp=>{
-                town_names.forEach(name => {
-                    const town_idx = nlp.text.search(RegExp(`${name}`,"i"))
-                    if(town_idx != -1){
-                    zips_set.push(town_idx)
-                    console.log("added")
+                if(town_names.some(substring => nlp.text.toLowerCase().includes(substring))){ //consider better search
+                    console.log("found")
                 }
-                })
             }) 
             .catch(reason=>{
                 console.log(reason);
             })  
         })
+
         console.log(zips_set)
 
         
